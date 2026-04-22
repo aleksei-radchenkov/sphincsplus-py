a571a56bac0dbf35113ebabc9d5b4a4f564d3d27
Joshua Hanbury <z5587781@ad.unsw.edu.au>
2026-04-22 18:37:24 +1000

    .

--- benchmarking/bench2.py
+++ benchmarking/bench2.py
@@ -24,6 +24,12 @@ class TestCase(NamedTuple):
     m: int
 
 test_cases = [
+    TestCase("demo", 16, 8, 2, 4, 4, 16, 24),
+    TestCase("SPHINCS+-128s", 16, 63, 7, 12, 14, 16, 133),
+    TestCase("SPHINCS+-128f", 16, 66, 22, 6, 33, 16, 128),
+    TestCase("SPHINCS+-192s", 24, 63, 7, 14, 17, 16, 193),
+    TestCase("SPHINCS+-192f", 24, 66, 22, 8, 33, 16, 194),
+    TestCase("SPHINCS+-256s", 32, 64, 8, 14, 22, 16, 255),
     TestCase("SPHINCS+-256f", 32, 68, 17, 9, 35, 16, 255),
 ]
 
