b3ed36ffedc1de74e52ed7a785d5fb00ae59eb61
Callum Reilly <z5592665@ad.unsw.edu.au>
2026-04-23 09:12:56 +1000

    add parameterized sphincs tests

--- tests/test_sphincs.py
+++ tests/test_sphincs.py
@@ -2,85 +2,100 @@ from sphincsplus import sphincs
 
 import pytest
 import secrets
+from collections import namedtuple
 
-N = 16
-H = 8
-D = 2
-A = 4
-K = 4
-W = 16
-M = K * A + H
+TestCase = namedtuple('TestCase', ['name', 'n', 'h', 'd', 'a', 'k', 'w', 'm'])
+
+test_cases = [
+    TestCase("SPHINCS+-128s", 16, 63, 7, 12, 14, 16, 14*12+63),
+    TestCase("SPHINCS+-128f", 16, 66, 22, 6, 33, 16, 33*6+66),
+    TestCase("SPHINCS+-192s", 24, 63, 7, 14, 17, 16, 17*14+63),
+    TestCase("SPHINCS+-192f", 24, 66, 22, 8, 33, 16, 33*8+66),
+    TestCase("SPHINCS+-256s", 32, 64, 8, 14, 22, 16, 22*14+64),
+    TestCase("SPHINCS+-256f", 32, 68, 17, 9, 35, 16, 35*9+68),
+]
 
 msg = b"Hello, World! and some more text"
 
 
-@pytest.fixture
-def keypair():
-    return sphincs.keygen(N, H, D, A, K, W, M)
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_sign_verify(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, pk = sphincs.keygen(n, h, d, a, k, w, m)
+    sig = sphincs.sign(msg, sk, n, h, d, a, k, w, m)
+
+    assert sphincs.verify(msg, sig, pk, n, h, d, a, k, w, m)
 
 
-def test_sign_verify(keypair):
-    sk, pk = keypair
-    sig = sphincs.sign(msg, sk, N, H, D, A, K, W, M)
-
-    assert sphincs.verify(msg, sig, pk, N, H, D, A, K, W, M)
-
-
-def test_wrong_message_fail(keypair):
-    sk, pk = keypair
-    sig = sphincs.sign(msg, sk, N, H, D, A, K, W, M)
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_wrong_message_fail(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, pk = sphincs.keygen(n, h, d, a, k, w, m)
+    sig = sphincs.sign(msg, sk, n, h, d, a, k, w, m)
 
     wrong_msg = b"This is a bs message."
 
-    assert not sphincs.verify(wrong_msg, sig, pk, N, H, D, A, K, W, M)
+    assert not sphincs.verify(wrong_msg, sig, pk, n, h, d, a, k, w, m)
 
 
-def test_wrong_pk_fail(keypair):
-    sk, _ = keypair
-    sig = sphincs.sign(msg, sk, N, H, D, A, K, W, M)
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_wrong_pk_fail(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, _ = sphincs.keygen(n, h, d, a, k, w, m)
+    sig = sphincs.sign(msg, sk, n, h, d, a, k, w, m)
 
     assert not sphincs.verify(
-        msg, sig, secrets.token_bytes(2 * N), N, H, D, A, K, W, M)
+        msg, sig, secrets.token_bytes(2 * n), n, h, d, a, k, w, m)
 
 
-def test_bad_sig_fail(keypair):
-    sk, pk = keypair
-    sig = sphincs.sign(msg, sk, N, H, D, A, K, W, M)
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_bad_sig_fail(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, pk = sphincs.keygen(n, h, d, a, k, w, m)
+    sig = sphincs.sign(msg, sk, n, h, d, a, k, w, m)
 
     sig_bad = bytearray(sig)
     sig_bad[0] &= 0xaa
 
-    assert not sphincs.verify(msg, bytes(sig_bad), pk, N, H, D, A, K, W, M)
+    assert not sphincs.verify(msg, bytes(sig_bad), pk, n, h, d, a, k, w, m)
 
 
-def test_deterministic(keypair):
-    sk, _ = keypair
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_deterministic(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, _ = sphincs.keygen(n, h, d, a, k, w, m)
 
-    sig1 = sphincs.sign(msg, sk, N, H, D, A, K, W, M, rand=False)
-    sig2 = sphincs.sign(msg, sk, N, H, D, A, K, W, M, rand=False)
+    sig1 = sphincs.sign(msg, sk, n, h, d, a, k, w, m, rand=False)
+    sig2 = sphincs.sign(msg, sk, n, h, d, a, k, w, m, rand=False)
 
     assert sig1 == sig2
 
 
-def test_random_different_fail(keypair):
-    sk, _ = keypair
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_random_different_fail(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, _ = sphincs.keygen(n, h, d, a, k, w, m)
 
-    sig1 = sphincs.sign(msg, sk, N, H, D, A, K, W, M, rand=True)
-    sig2 = sphincs.sign(msg, sk, N, H, D, A, K, W, M, rand=True)
+    sig1 = sphincs.sign(msg, sk, n, h, d, a, k, w, m, rand=True)
+    sig2 = sphincs.sign(msg, sk, n, h, d, a, k, w, m, rand=True)
 
     assert sig1 != sig2
 
 
-def test_random_verify(keypair):
-    sk, pk = keypair
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_random_verify(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, pk = sphincs.keygen(n, h, d, a, k, w, m)
 
     for _ in range(3):
-        sig = sphincs.sign(msg, sk, N, H, D, A, K, W, M, rand=True)
-        assert sphincs.verify(msg, sig, pk, N, H, D, A, K, W, M)
+        sig = sphincs.sign(msg, sk, n, h, d, a, k, w, m, rand=True)
+        assert sphincs.verify(msg, sig, pk, n, h, d, a, k, w, m)
 
 
-def test_multiple(keypair):
-    sk, pk = keypair
+@pytest.mark.parametrize("test_case", test_cases, ids=lambda tc: tc.name)
+def test_multiple(test_case):
+    _, n, h, d, a, k, w, m = test_case
+    sk, pk = sphincs.keygen(n, h, d, a, k, w, m)
 
     messages = [
         b"",
@@ -89,7 +104,7 @@ def test_multiple(keypair):
         secrets.token_bytes(1000),
     ]
 
-    for m in messages:
-        sig = sphincs.sign(m, sk, N, H, D, A, K, W, M)
+    for message in messages:
+        sig = sphincs.sign(message, sk, n, h, d, a, k, w, m)
 
-        assert sphincs.verify(m, sig, pk, N, H, D, A, K, W, M)
+        assert sphincs.verify(message, sig, pk, n, h, d, a, k, w, m)
