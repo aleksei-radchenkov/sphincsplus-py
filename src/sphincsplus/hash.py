384408c9732a620a0fe19560c0eec85ae3a0a950
Aleksei Radchenkov <z5591304@ad.unsw.edu.au>
2026-04-22 15:13:03 +1000

    Added header comment for hash.py, with relevant formulas from the spec, also some non-functional changes so it more clearly aligns with the spec.

--- src/sphincsplus/hash.py
+++ src/sphincsplus/hash.py
@@ -1,38 +1,54 @@
+# Hash functions (SHAKE-256-robust)
+# (Defined in https://sphincs.org/data/sphincs+-round3-specification.pdf, 7.2.1,
+# pg. 38-39)
+#
+# h_msg = SHAKE_256(R || PK.seed || PK.root || M, 8m)
+# prf = SHAKE_256(SEED || ADRS, 8n)
+# prf_msg = SHAKE_256(SK.prf || OptRand || M, 8n)
+#
+# Tweak-funcs for the robust variant:
+# f = SHAKE_256(PK.seed || ADRS || M1 (masked) 8n)
+# h = SHAKE_256(PK.seed || ADRS || M1 (masked) || M2 (masked), 8n)
+# tl = SHAKE_256(PK.seed || ADRS || M (masked), 8n)
+#
+# Mask M = m xor SHAKE_256(PK.seed || ADRS, l)
+
 import hashlib
-import math
 
 
-def _hash(inp: bytes, length: int) -> bytes:
-    return hashlib.shake_256(inp).digest(length)
+def _hash(inp: bytes, out_len: int) -> bytes:
+    return hashlib.shake_256(inp).digest(out_len)
 
 
-def _mask_gen(pk_seed: bytes, adrs: bytes, inp: bytes) -> bytes:
-    mask = _hash(pk_seed + adrs, len(inp))
-    return bytes(x ^ y for x, y in zip(inp, mask))
+def _mask_gen(pk_seed: bytes, adrs: bytes, m: bytes) -> bytes:
+    mask = _hash(pk_seed + adrs, len(m))
+    return bytes(x ^ y for x, y in zip(m, mask))
 
 
-def h_msg(r: bytes, pk_seed: bytes, pk_root: bytes, msg: bytes, m: int) -> bytes:
-    return _hash(r + pk_seed + pk_root + msg, math.ceil(m / 8))
+def _h_msg(r: bytes, pk_seed: bytes, pk_root: bytes, msg: bytes, m: int) -> bytes:
+    return _hash(r + pk_seed + pk_root + msg, m)
 
 
-def prf(sk_seed: bytes, adrs: bytes) -> bytes:
+def _prf(sk_seed: bytes, adrs: bytes) -> bytes:
     return _hash(sk_seed + adrs, len(sk_seed))
 
 
-def prf_msg(sk_prf: bytes, opt_rand: bytes, msg: bytes) -> bytes:
+def _prf_msg(sk_prf: bytes, opt_rand: bytes, msg: bytes) -> bytes:
     return _hash(sk_prf + opt_rand + msg, len(sk_prf))
 
 
-def f(pk_seed: bytes, adrs: bytes, inp: bytes) -> bytes:
-    n = len(pk_seed)
-    return _hash(pk_seed + adrs + _mask_gen(pk_seed, adrs, inp), n)
+def _f(pk_seed: bytes, adrs: bytes, m1: bytes) -> bytes:
+    mask = _mask_gen(pk_seed, adrs, m1)
+    return _hash(pk_seed + adrs + mask, len(pk_seed))
 
 
-def h(pk_seed: bytes, adrs: bytes, inp: bytes) -> bytes:
-    n = len(pk_seed)
-    return _hash(pk_seed + adrs + _mask_gen(pk_seed, adrs, inp), n)
+def _h(pk_seed: bytes, adrs: bytes, m1: bytes, m2: bytes) -> bytes:
+    mask_m1 = _mask_gen(pk_seed, adrs, m1)
+    mask_m2 = _mask_gen(pk_seed, adrs, m2)
+
+    return _hash(pk_seed + adrs + mask_m1 + mask_m2, len(pk_seed))
 
 
-def th(pk_seed: bytes, adrs: bytes, inp: bytes) -> bytes:
-    n = len(pk_seed)
-    return _hash(pk_seed + adrs + _mask_gen(pk_seed, adrs, inp), n)
+def _tl(pk_seed: bytes, adrs: bytes, m: bytes) -> bytes:
+    mask = _mask_gen(pk_seed, adrs, m)
+    return _hash(pk_seed + adrs + mask, len(pk_seed))
